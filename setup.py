526b432cda0babf07c16fd0823401b7d1882622c
Tan Shih Jen <jen910604@gmail.com>
2025-07-13 12:21:53 +0800

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dfqa",
-    version="0.1.1",
+    version="0.1.2",
     description="A Python library for assessing the data quality in pandas DataFrames.",
     author="Tan Shih Jen",      
     license="MIT",
